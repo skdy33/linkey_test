92f15f1b7099b1c5f15d647a208295a143100fa2
skdy33 <skdy3333@gmail.com>
2018-10-03 19:45:34 +0900

    debug main

---
+++ main.py
@@ -0,0 +1,13 @@
+import http.server
+import socketserver
+
+if __name__=="__main__":
+    PORT = 8000
+
+    Handler = http.server.SimpleHTTPRequestHandler
+
+    httpd = socketserver.TCPServer(("", PORT), Handler)
+
+    print("serving at port", PORT)
+    httpd.serve_forever()
+
